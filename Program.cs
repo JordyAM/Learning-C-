73f4f9efc610a087774cbf8ee714bfa2d537402d
JordyAM <JAlexanderMcLean@Gmail.com>
2022-09-15 19:46:02 -0400

    first C sharp program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        // string message = "Hello World!!";
+
+        Console.WriteLine("Who are You?"); 
+        var name = Console.ReadLine(); 
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"\nHello, {name}, on {currentDate:d} at {currentDate:t}!");
+        Console.Write($"\nPress any key to exit...");
+        Console.ReadKey(true);
+    }
+    }
+}
