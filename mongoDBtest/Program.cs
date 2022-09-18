718c29b0d15a052555ab3917dd628df7ad50c0b8
JordyAM <JAlexanderMcLean@Gmail.com>
2022-09-17 21:38:16 -0400

    testing mongoDB connection

---
+++ mongoDBtest/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using MongoDB.Driver; 
+
+namespace mongoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://dbuser:9XWrtOYIy3vm9deS@cluster0.nztui.mongodb.net/?retryWrites=true&w=majority");
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databasses on this server is: ");
+            foreach(var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+    }
+}
